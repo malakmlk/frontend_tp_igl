70fc77b589898f2e2d8bc36fbb54b17308cedde2
malakmlk <hm_saiem@esi.dz>
2019-11-26 09:14:28 +0100

    les notes

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{ Component } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -13,85 +13,168 @@ import About from './About';
 
 
 
-const useStyles = makeStyles({
-  root: {
-    width: '50%',
-    overflowX: 'auto',
+
+export default class Note extends Component {
+ 
+
+  constructor(props) {
+      super(props);
+  
+      /*this.onChangeCF = this.onChangeCF.bind(this);
+      this.onChangeCI = this.onChangeCI.bind(this);
+      this.onChangeCC = this.onChangeCC.bind(this);
+      this.onChangeMoyenne = this.onChangemoyenne.bind(this);
+      this.onChangeModule= this.onChangeModule.bind(this);
+      this.onSubmit = this.onSubmit.bind(this);*/
+  
+      this.state = {
+          CF: '',
+          CI: '',
+          CC: '',
+          moyenne:'',
+          module:'',
+      }
+  }
+  
+
+  
+  onChangeCF(e) {
+      this.setState({
+          CF: e.target.value
+      });
+  }
+  
+  onChangeCI(e) {
+      this.setState({
+          CI: e.target.value
+      });
+  }
+  
+  onChangeCC(e) {
+      this.setState({
+          CC: e.target.value
+      });
+  }
+  
+  onChangeMoyenne(e) {
+      this.setState({
+          moyenne: e.target.value
+      });
+  }
+  onChangeModule(e) {
+    this.setState({
+        module: e.target.value
+    });
+  }
+  
+  onSubmit(e) {
+      e.preventDefault();
+      const obj = {
+          CF: this.state.CF,
+          CI: this.state.CI,
+          CC: this.state.CC,
+          moyenne: this.state.moyenne,
+          module: this.state.module,
+  
+      };
+    /*  console.log(obj);
+      axios.post('http://localhost:4000/todos/update/'+this.props.match.params.id, obj)
+          .then(res => console.log(res.data));*/
+      
+      this.props.history.push('/');
+  }
+  
+  render() {
+    const classes = useStyles();
+    const useStyles = makeStyles({
+      root: {
+        width: '50%',
+        overflowX: 'auto',
+        
+      },
+      table: {
+        minWidth: 390,
+        marginLeft:'800 px',
+        marginTop:'1000px',
+        width:"50px",
+        shadow :90,
+      },
+    });
+     function createData(Module, CC, CI, CF, Moyenne) {
+      return { Module, CC, CI, CF, Moyenne };
     
-  },
-  table: {
-    minWidth: 390,
-    marginLeft:'800 px',
-    marginTop:'1000px',
-    width:"50px",
-    shadow :90,
-  },
-});
-
-function createData(Module, CC, CI, CF, Moyenne) {
-  return { Module, CC, CI, CF, Moyenne };
-}
-
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-  
-];
-
-export default function SimpleTable() {
-  const classes = useStyles();
-  
-  return (
-
-    <Layout>
-     <About></About>
-
-    <React.Fragment>
-    <Container fixed shadow={30}>
-     
+    }
     
-                    <Paper classModule={classes.root}  style ={{marginTop:'80px' }}>
-                       
-
-                    <Table classModule={classes.table} aria-label="simple table">
-                        <TableHead>
-                        <TableRow>
-                            <TableCell>Module</TableCell>
-                            <TableCell align="right">CC</TableCell>
-                            <TableCell align="right">CI</TableCell>
-                            <TableCell align="right">CF</TableCell>
-                            <TableCell align="right">Moyenne</TableCell>
-                        </TableRow>
-                        </TableHead>
-                        <TableBody>
-                        {rows.map(row => (
-                            <TableRow key={row.Module}>
-                            <TableCell component="th" scope="row">
-                                {row.Module}
-                            </TableCell>
-                            <TableCell align="right">{row.CC}</TableCell>
-                            <TableCell align="right">{row.CI}</TableCell>
-                            <TableCell align="right">{row.CF}</TableCell>
-                            <TableCell align="right">{row.Moyenne}</TableCell>
-                            </TableRow>
-                        ))}
-                        </TableBody>
-                    </Table>
-                    </Paper>
-                    
-    </Container>
-    </React.Fragment>
-    </Layout>
-  
-  
+    const rows = [
+      createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+      createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+      createData('Eclair', 262, 16.0, 24, 6.0),
+      createData('Cupcake', 305, 3.7, 67, 4.3),
+      createData('Gingerbread', 356, 16.0, 49, 3.9),
+      createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+      createData('Eclair', 262, 16.0, 24, 6.0),
+      createData('Cupcake', 305, 3.7, 67, 4.3),
+      createData('Gingerbread', 356, 16.0, 49, 3.9),
+      
+    ];
+      return (
     
+
+        <Layout>
+        <About></About>
    
-  );
-}
+       <React.Fragment>
+       <Container fixed shadow={30}>
+        
+       
+                       <Paper classModule={classes.root}  style ={{marginTop:'80px' }}>
+                          
+   
+                       <Table classModule={classes.table} aria-label="simple table">
+                           <TableHead>
+                           <TableRow>
+                               <TableCell>Module</TableCell>
+                               <TableCell align="right">CC</TableCell>
+                               <TableCell align="right">CI</TableCell>
+                               <TableCell align="right">CF</TableCell>
+                               <TableCell align="right">Moyenne</TableCell>
+                           </TableRow>
+                           </TableHead>
+                           <TableBody>
+                           {rows.map(row => (
+                               <TableRow key={row.Module}>
+                               <TableCell component="th" scope="row">
+                                   {row.Module}
+                               </TableCell>
+                               <TableCell align="right">{row.CC}</TableCell>
+                               <TableCell align="right">{row.CI}</TableCell>
+                               <TableCell align="right">{row.CF}</TableCell>
+                               <TableCell align="right">{row.Moyenne}</TableCell>
+                               </TableRow>
+                           ))}
+                           </TableBody>
+                       </Table>
+                       </Paper>
+                       
+       </Container>
+       </React.Fragment>
+       </Layout>
+         
+      )
+  }
+  }
+  
+  
+  
+
+
+
+
+  
+  
+ 
+
+   
+  
+  
+    
--- src/components/coucou.js
+++ src/components/coucou.js
@@ -1,27 +1,155 @@
-import React, { Component } from 'react'
 
-export class coucou extends Component {
+import React, { Component } from 'react';
+import axios from 'axios';
 
+export default class EditTodo extends Component {
 
-    constructor(props){
-        super(props);
-        this.state = 
-        { CF: '',
-          CC:'',
-          CI:'',
-          MODULE:'',
-          Moyenne:'',
-       };
+constructor(props) {
+    super(props);
 
+    /*this.onChangeCF = this.onChangeCF.bind(this);
+    this.onChangeCI = this.onChangeCI.bind(this);
+    this.onChangeCC = this.onChangeCC.bind(this);
+    this.onChangeMoyenne = this.onChangemoyenne.bind(this);
+    this.onChangeModule= this.onChangeModule.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);*/
+
+    this.state = {
+        CF: '',
+        CI: '',
+        CC: '',
+        moyenne:'',
+        module:'',
     }
-
-  render() {
-    return (
-      <div>
-        
-      </div>
-    )
-  }
 }
+
+
+
+onChangeCF(e) {
+    this.setState({
+        CF: e.target.value
+    });
+}
+
+onChangeCI(e) {
+    this.setState({
+        CI: e.target.value
+    });
+}
+
+onChangeCC(e) {
+    this.setState({
+        CC: e.target.value
+    });
+}
+
+onChangeMoyenne(e) {
+    this.setState({
+        moyenne: e.target.value
+    });
+}
+onChangeModule(e) {
+  this.setState({
+      module: e.target.value
+  });
+}
+
+onSubmit(e) {
+    e.preventDefault();
+    const obj = {
+        todo_description: this.state.todo_description,
+        todo_responsible: this.state.todo_responsible,
+        todo_priority: this.state.todo_priority,
+        todo_completed: this.state.todo_completed
+    };
+    console.log(obj);
+    axios.post('http://localhost:4000/todos/update/'+this.props.match.params.id, obj)
+        .then(res => console.log(res.data));
+    
+    this.props.history.push('/');
+}
+
+render() {
+    return (
+        <div>
+            <h3 align="center">Update Todo</h3>
+            <form onSubmit={this.onSubmit}>
+                <div className="form-group"> 
+                    <label>Description: </label>
+                    <input  type="text"
+                            className="form-control"
+                            value={this.state.todo_description}
+                            onChange={this.onChangeCF}
+                            />
+                </div>
+                <div className="form-group">
+                    <label>Responsible: </label>
+                    <input 
+                            type="text" 
+                            className="form-control"
+                            value={this.state.todo_responsible}
+                            onChange={this.onChangeCI}
+                            />
+                </div>
+                <div className="form-group">
+                    <div className="form-check form-check-inline">
+                        <input  className="form-check-input" 
+                                type="radio" 
+                                name="priorityOptions" 
+                                id="priorityLow" 
+                                value="Low"
+                                checked={this.state.todo_priority==='Low'} 
+                                onChange={this.onChangeCC}
+                                />
+                        <label className="form-check-label">Low</label>
+                    </div>
+                    <div className="form-check form-check-inline">
+                        <input  className="form-check-input" 
+                                type="radio" 
+                                name="priorityOptions" 
+                                id="priorityMedium" 
+                                value="Medium" 
+                                checked={this.state.todo_priority==='Medium'} 
+                                onChange={this.onChangeCC}
+                                />
+                        <label className="form-check-label">Medium</label>
+                    </div>
+                    <div className="form-check form-check-inline">
+                        <input  className="form-check-input" 
+                                type="radio" 
+                                name="priorityOptions" 
+                                id="priorityHigh" 
+                                value="High" 
+                                checked={this.state.todo_priority==='High'} 
+                                onChange={this.onChangeCC}
+                                />
+                        <label className="form-check-label">High</label>
+                    </div>
+                </div>
+                <div className="form-check">
+                    <input  className="form-check-input"
+                            id="completedCheckbox"
+                            type="checkbox"
+                            name="completedCheckbox"
+                            onChange={this.onChangemoyenne}
+                            checked={this.state.todo_completed}
+                            value={this.state.todo_completed}
+                            />
+                    <label className="form-check-label" htmlFor="completedCheckbox">
+                        Completed
+                    </label>                        
+                </div>
+
+                <br />
+
+                <div className="form-group">
+                    <input type="submit" value="Update Todo" className="btn btn-primary" />
+                </div>
+            </form>
+        </div>
+    )
+}
+}
+
 
 export default coucou
